fc39e7999126523d96e5989bc73b4a99f57fb8dc
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-08-27 14:25:27 +0200

    Add temporary graphic to carousel, Add hash.js file, Add Init hash.js option in script.js file.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  hash: true
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
-  // options
 });
