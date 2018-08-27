6e8570377bfe4a4d130bfc55ffcad1d18323b7d0
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-08-27 15:34:29 +0200

    Add progress bar do index.html, Add progress bar styles, Add progress bar function to script.js.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,15 @@ var flkty = new Flickity( elem, {
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
 });
+
+// restart function
 var restart = document.querySelector('.restart');
  restart.addEventListener("click", function(event){
       flkty.select(0);
   });
+// progress bar
+var progressBar = document.querySelector('.progress-bar')
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
